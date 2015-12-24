c51eb88dee6273f8f55d6177616ab1eac0277e1d
Давид <budnik.david@mail.ru>
2015-12-24 16:43:31 +0600

    New classes for IFormatter

---
+++ src/com/company/formatter/CloseBraceFormatter.java
@@ -0,0 +1,14 @@
+package com.company.formatter;
+
+import com.company.destination.IDestination;
+import com.company.source.ISource;
+
+/**
+ * Created by Давид on 24.12.2015.
+ */
+public class CloseBraceFormatter implements IFormatter {
+    @Override
+    public void format(ISource source, IDestination destination) throws FormatException {
+
+    }
+}
---
+++ src/com/company/formatter/SpaceFormatter.java
@@ -0,0 +1,14 @@
+package com.company.formatter;
+
+import com.company.destination.IDestination;
+import com.company.source.ISource;
+
+/**
+ * Created by Давид on 24.12.2015.
+ */
+public class SpaceFormatter implements IFormatter {
+    @Override
+    public void format(ISource source, IDestination destination) throws FormatException {
+
+    }
+}
